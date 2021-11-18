46d3f433ee98f56a3a55c324632a74b0378f0dfa
GlebYoutuber <73891760+GlebYoutuber@users.noreply.github.com>
2021-11-18 22:47:10 +0300

    Windows 11 Bypass Secure Boot and TPM 2.0

---
+++ src/Windows11Bypass/Windows11BypassSecureBootTPM.Designer.cs
@@ -0,0 +1,55 @@
+﻿
+namespace Windows11Bypass
+{
+    partial class Windows11BypassSecureBootTPM
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Windows11BypassSecureBootTPM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(28, 21);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Windows11BypassSecureBootTPM";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ src/Windows11Bypass/Windows11BypassSecureBootTPM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Windows11Bypass
+{
+    public partial class Windows11BypassSecureBootTPM : Form
+    {
+        public Windows11BypassSecureBootTPM()
+        {
+            InitializeComponent();
+            if (MessageBox.Show("Do you want Bypass Windows 11 Secure Boot Check and TPM 2.0?", "Bypass Windows 11 Secure Boot Check and TPM 2.0", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+                RegistryKey bypasstpmandsecurebootcheck = Registry.LocalMachine.CreateSubKey(@"SYSTEM\Setup\LabConfig");
+                bypasstpmandsecurebootcheck.SetValue("BypassTPMCheck", "1", RegistryValueKind.DWord);
+                bypasstpmandsecurebootcheck.SetValue("BypassSecureBootCheck", "1", RegistryValueKind.DWord);
+                RegistryKey BypassCPUCheckandTPMCheckINMoSetup = Registry.LocalMachine.CreateSubKey(@"SYSTEM\Setup\MoSetup");
+                BypassCPUCheckandTPMCheckINMoSetup.SetValue("AllowUpgradesWithUnsupportedTPMOrCPU", "1", RegistryValueKind.DWord);
+                MessageBox.Show("Done.");
+            }
+            Application.Exit();
+        }
+    }
+}
